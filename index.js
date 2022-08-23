9c8c4c1c1d647cb89bd1ed908b4c0cd20474bfca
timo.scheer <timo.scheer@bytediver.de>
2022-08-23 19:01:55 +0200

    enabling json response

--- index.js
+++ index.js
@@ -3,8 +3,15 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var test = 'Das ist ein langer text und der ist cool'
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/json', function(req, res){
+    res.json({ text: test});
+});
 
 app.listen(8000);
 console.log('listening on port 8000');
