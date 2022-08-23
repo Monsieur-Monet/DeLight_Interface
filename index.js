596c0464b552b8dc92c3c3cec910e5a3e7cc69c3
timo.scheer <timo.scheer@bytediver.de>
2022-08-23 19:07:11 +0200

    minor changes

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ var path = require('path');
 var app = express();
 var test = 'Das ist ein langer text und der ist cool'
 
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/json', function(req, res){
     res.json({ text: test});
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.listen(8000);
 console.log('listening on port 8000');
