5d8dbfcfc9487dd3fe1c12d3adf823e9aef7cc68
timo.scheer <timo.scheer@bytediver.de>
2022-08-24 17:29:43 +0200

    adding server response

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 'use strict'
-
 var express = require('express');
 var path = require('path');
 var app = express();
-var test = 'Das ist ein langer text und der ist cool'
+const fs = require('fs');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/json', function(req, res){
-    res.json({ text: test});
+fs.readFile('colorFile.json', (err, data) => {
+    if (err) throw err;
+    let jsoncolor = JSON.parse(data);
+    res.json(jsoncolor);
 });
+});
+
+app.post('/test', function(req, res){
+let colorValue = { color: req.body.color};
+let data = JSON.stringify(colorValue);
+fs.writeFileSync('colorFile.json', data)
+res.sendStatus(200)
+})
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.listen(8000);
 console.log('listening on port 8000');
+
 
